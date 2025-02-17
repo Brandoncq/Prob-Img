0d24a9f0aae434b225cf2f1bf78325e76ee4c8dd
Davis <brandoncq036@hotmail.com>
2025-02-17 15:39:14 -0500

    Agregando texto

--- src/pages/ruta-1/page.tsx
+++ src/pages/ruta-1/page.tsx
@@ -96,6 +96,22 @@ function ImagePage({ images }: ImagePageProps) {
             </div>
           </div>
         </div>
+        <div className="w-full flex flex-col h-lvh justify-center items-center p-10 bg-[url(/tinified/bg-1.jpg)] bg-no-repeat bg-cover bg-center">
+          <div className="w-full lg:w-1/2">
+            <p className="text-sm lg:text-lg font-light text-center">
+              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to make a
+              type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially
+              unchanged. It was popularised in the 1960s with the release of
+              Letraset sheets containing Lorem Ipsum passages, and more recently
+              with desktop publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum.
+            </p>
+          </div>
+        </div>
         <div className="w-full p-2">
           <GridImg images={gridImages} columns={4} mobileColumns={2} />
         </div>
